d13d09ff016fa0f314f55571f22cede0bb76d344
Emilio04ca <cordovacarlos210@gmail.com>
2021-06-23 11:10:54 -0500

    actulizacion

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -5,15 +5,18 @@ const Valor_Squema = require('../model/Esquema_Tabla');
 
 router.get('/', async (req, res) => {
     const tablas = await Valor_Squema.find();
-    console.log(tablas);
-    res.render('Registros.ejs',{
+    //console.log(tablas);
+   //res.json(tablas);
+     res.render('Registros copy.ejs',{
         tablas
     });
 });
 router.post('/add', async (req, res) => {
     const tabla = new Valor_Squema(req.body);
     await tabla.save();
-    res.redirect('/');
+    //res.json(tabla)
+   // res.redirect('/');
+   
 });
 
 module.exports = router;
